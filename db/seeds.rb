2c9ed6b7f31b141a56472622569c73e8c4a4658e
Nico Proto <nicolasproto@gmail.com>
2019-08-08 11:09:49 +0200

    Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating 4 fake instances"
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Somewhere over the rainbow',
+  address: 'I dont know 123',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 25,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'The beach',
+  address: 'Barceloneta3',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 10,
+  number_of_guests: 100
+)
+
+Flat.create!(
+  name: 'Rainbow place',
+  address: 'I dont 123',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 25,
+  number_of_guests: 5
+)
+
+puts "Done"
